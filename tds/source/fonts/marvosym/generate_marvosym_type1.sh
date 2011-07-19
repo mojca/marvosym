a3b96971c722934afaf8dceab36de076d5df9a1b
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2011-07-19 22:45:45 +0200

    fix previous slips

--- tds/source/fonts/marvosym/generate_marvosym_type1.sh
+++ tds/source/fonts/marvosym/generate_marvosym_type1.sh
@@ -27,11 +27,11 @@ write_comment "create type1 fonts"
 cd $TDS/fonts/truetype/public/marvosym
 echo "cd $PWD"
 run_command "ttf2pt1 -b marvosym.ttf"
-run_command "mv marvosym.afm $tds/fonts/afm/public/marvosym"
-run_command "mv marvosym.pfb $tds/fonts/type1/public/marvosym"
+run_command "mv marvosym.afm $TDS/fonts/afm/public/marvosym"
+run_command "mv marvosym.pfb $TDS/fonts/type1/public/marvosym"
 
 write_comment "create tex metrics"
-cd $tds/fonts/afm/public/marvosym
+cd $TDS/fonts/afm/public/marvosym
 echo "cd $PWD"
 run_command "afm2tfm marvosym.afm"
-run_command "mv marvosym.tfm $tds/fonts/tfm/public/marvosym/umvs.tfm"
+run_command "mv marvosym.tfm $TDS/fonts/tfm/public/marvosym/umvs.tfm"
