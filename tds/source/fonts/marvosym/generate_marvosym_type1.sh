cc0e03f3b3d1368eafec30335de3d853d9084cdb
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2011-07-19 22:42:37 +0200

    add ttf and regenerate type1 from ttf

---
+++ tds/source/fonts/marvosym/generate_marvosym_type1.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+dir=$PWD
+cd ../../../
+TDS=$PWD
+
+write_comment()
+{
+	echo
+	echo "# $1"
+	echo
+}
+
+run_command()
+{
+	echo
+	echo ">>> running: '$1'"
+	if $1; then
+		echo "    OK"
+	else
+		echo "    failed"
+		return -1
+	fi
+}
+
+write_comment "create type1 fonts"
+cd $TDS/fonts/truetype/public/marvosym
+echo "cd $PWD"
+run_command "ttf2pt1 -b marvosym.ttf"
+run_command "mv marvosym.afm $tds/fonts/afm/public/marvosym"
+run_command "mv marvosym.pfb $tds/fonts/type1/public/marvosym"
+
+write_comment "create tex metrics"
+cd $tds/fonts/afm/public/marvosym
+echo "cd $PWD"
+run_command "afm2tfm marvosym.afm"
+run_command "mv marvosym.tfm $tds/fonts/tfm/public/marvosym/umvs.tfm"
